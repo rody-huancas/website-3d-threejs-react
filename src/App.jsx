1c27a41f796b6159e10cc402e13efb45367b9139
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-08 22:09:46 -0500

    Add: pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
+
 const App = () => {
   return (
-    <div>App</div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
