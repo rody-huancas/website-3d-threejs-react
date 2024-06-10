1b13709e30f87e19fe5f78d537d201d85c9ee70b
Rody Huancas <rodyhuancas.04@gmail.com>
2024-06-09 23:24:03 -0500

    Add: home page and valtio for state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,53 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useSnapshot } from "valtio";
+import { AnimatePresence, motion } from "framer-motion";
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from "../config/motion";
+import state from "../store";
+import { CustomButton } from "../components"
 
-export default Home
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.div className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:bloc hidden" /> DO IT.
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create you unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagination</strong>{" "}
+                and define your own style.
+              </p>
+
+              <CustomButton 
+                type="filled"
+                title="Custom It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
